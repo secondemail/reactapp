de44019fc8733e38f877006bde2d4d358a86fc15
secondemail <166629198+secondemail@users.noreply.github.com>
2024-04-17 12:35:46 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer'
-import { composeWithDevTools } from 'redux-devtools-extension'
+/*import { composeWithDevTools } from 'redux-devtools-extension'*/
 const initailState = {}
 
 const meddleware = [thunk]
 
-const store = createStore(rootReducer, initailState, composeWithDevTools(applyMiddleware(...meddleware)))
+const store = createStore(rootReducer, initailState,applyMiddleware(...meddleware))
 
 export default store
